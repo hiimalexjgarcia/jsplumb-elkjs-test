514449e52952ea36c2e866477eae31fbfe5b36e2
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2024-02-29 21:10:33 -0700

    Added elkjs, set up layered layout for nodes.

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,54 @@
 import './style.css'
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = ``
-
-import { newInstance } from "@jsplumb/browser-ui"
+import { newInstance } from '@jsplumb/browser-ui'
+import ELK from 'elkjs/lib/elk.bundled.js'
 
 const instance = newInstance({
   container: document.querySelector<HTMLDivElement>('#app')!,
 });
 
-const a = document.createElement("div")
-a.className = "node"
-a.innerHTML = "a";
-document.querySelector<HTMLDivElement>('#app')?.appendChild(a)
-
-const b = document.createElement("div")
-b.className = "node"
-b.innerHTML = "b";
-document.querySelector<HTMLDivElement>('#app')?.appendChild(b)
-
-const c = document.createElement("div")
-c.className = "node"
-c.innerHTML = "c";
-document.querySelector<HTMLDivElement>('#app')?.appendChild(c)
-
-instance.connect({
-  source: a,
-  target: b,
-  anchor: { type: "Perimeter", options: { shape: "Rectangle" } },
-  endpoints: ["Blank", "Blank"],
-  overlays: [
-    { type: "Arrow", options: { location: 1 } },
+const graph = {
+  id: "root",
+  layoutOptions: { 'elk.algorithm': 'layered' },
+  children: [
+    { id: "n1", width: 100, height: 50 },
+    { id: "n2", width: 150, height: 50 },
+    { id: "n3", width: 100, height: 150 },
+    { id: "n4", width: 200, height: 150 }
+  ],
+  edges: [
+    { id: "e1", sources: [ "n1" ], targets: [ "n2" ] },
+    { id: "e2", sources: [ "n1" ], targets: [ "n3" ] },
+    { id: "e3", sources: [ "n2" ], targets: [ "n4" ] },
+    { id: "e4", sources: [ "n3" ], targets: [ "n4" ] }
   ]
-})
+}
 
-instance.connect({
-  source: b,
-  target: c,
-  anchor: { type: "Perimeter", options: { shape: "Rectangle" } },
-  endpoints: ["Blank", "Blank"],
-  overlays: [
-    { type: "Arrow", options: { location: 1 } },
-  ]
-})
+const elk = new ELK()
+
+elk.layout(graph)
+  .then((r) => {
+    console.log(r)
+    r.children?.forEach(c => {
+      const node = document.createElement("div")
+      node.id = c.id
+      node.className = "node"
+      node.innerHTML = c.id
+      node.style.top = `${c.y}px`
+      node.style.left = `${c.x}px`
+      node.style.width = `${c.width}px`
+      node.style.height = `${c.height}px`
+      document.querySelector<HTMLDivElement>('#app')?.appendChild(node)
+    })
+    r.edges?.forEach(e => {
+      instance.connect({
+        source: document.getElementById(e.sources[0]) || undefined,
+        target: document.getElementById(e.targets[0]) || undefined,
+        anchor: { type: "Perimeter", options: { shape: "Rectangle" } },
+        endpoints: ["Blank", "Blank"],
+        overlays: [
+          { type: "Arrow", options: { location: 1 } },
+        ]
+      })
+    })
+  })
+  .catch(console.error)
